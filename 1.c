e502f35fd48111f8116fd634b079e24f824c1c7d
Ch3rryRby <daniella.pratto@escola.pr.gov.br>
2025-03-12 09:44:03 -0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+- ler um numero N e um vetor de N inteiros
+- calcular a média dos valores lidos
+- imprimir essa média
+- imprimir os elementos do vetor que forem maiores que a média calculada
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    int numero, dobro;
+    scanf("%d", &numero);
+    dobro = numero * 2;
+    printf("dobro: %d", dobro);
+    return 0;
+    
+}
